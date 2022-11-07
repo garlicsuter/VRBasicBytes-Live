6cdf2597dfb37f267b0b99087d2bf148c5505ab3
Garlicsuter <markasuter@gmail.com>
2022-11-07 15:06:26 -0500

    minor updates to code

--- Assets/02 - Cornhole/Scripts/GameManager.cs
+++ Assets/02 - Cornhole/Scripts/GameManager.cs
@@ -11,15 +11,8 @@ public class GameManager : MonoBehaviour
     public int orangeRoundScoreValue = 0;
     public int blueRoundScoreValue = 0;
 
-    // Start is called before the first frame update
-    void Start()
+    void CalcRoundScore()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
